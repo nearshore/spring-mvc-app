204edc1623d0f190b5f56482a3c7987648144e20
Romel R. Sandoval <romel.sandoval@nearshoretechnology.com>
2017-04-10 06:06:18 -0500

    Base code

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+((nil . ((indent-tabs-mode . nil)
+         (tab-width . 2)
+         (fill-column . 80)
+         (c-basic-offset . 2)
+         ;;(java-mode . ((c-basic-offset . 2)))
+         (buffer-file-coding-system . utf-8-unix))))
